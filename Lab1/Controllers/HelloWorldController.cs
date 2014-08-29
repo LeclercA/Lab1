64d495b23ae6b2722709c01be894f92f29a32c6a
LeclercA <pinky7997@hotmail.ca>
2014-08-29 13:47:58 -0400

    Première page
    Première page du laboratoire : ajout de deux pages, HelloWorld et
    HelloWorld/Welcome

---
+++ Lab1/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public string Index()
+        {
+            return "Ceci est mon action par <b>défaut</b>...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Bonjour " + name + ", NumTimes is: " + numTimes);
+        }
+    }
+}
